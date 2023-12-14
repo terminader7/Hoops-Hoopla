870a6f84aff11b0f66d976a215b1ddba2357e3ed
Nader Ebrahim <nebrahim2424@gmail.com>
2023-12-14 09:39:41 -0800

    set up small bit of theme file, playing around with landing paige container layers

---
+++ config/theme.ts
@@ -0,0 +1,18 @@
+import {
+  Breakpoints,
+  darken,
+  lighten,
+  PaletteMode,
+  ThemeOptions,
+  PaletteOptions,
+} from "@mui/material";
+
+export const breakpoints: { values: Breakpoints["values"] } = {
+  values: {
+    xs: 0,
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,49 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Box, Typography } from "@mui/material";
+import InlineContainer from "../components/InlineContainer";
+import { green } from "@mui/material/colors";
+import { Global } from "@emotion/react";
 
 export default function Home() {
   return (
-    <Box>
-      <Typography variant="h2" fontWeight={600}>
-        Welcome to Hoops Hoopla!
-      </Typography>
-    </Box>
+    <>
+      <Global
+        styles={{
+          body: {
+            margin: 0,
+            padding: 0,
+          },
+          html: {
+            margin: 0,
+            padding: 0,
+          },
+        }}
+      />
+      <Box
+        sx={{
+          position: "relative",
+          height: "100vh",
+          backgroundColor: "black",
+          padding: 0,
+          margin: 0,
+        }}
+      >
+        <Box
+          sx={{
+            height: "100vh",
+            backgroundColor: "green",
+            marginX: "5rem",
+          }}
+        >
+          <Typography variant="h2" fontWeight={600}>
+            Welcome to Hoops Hoopla!
+          </Typography>
+          <InlineContainer sx={{ minWidth: "100px", backgroundColor: green }}>
+            <Typography>Hello</Typography>
+          </InlineContainer>
+        </Box>
+      </Box>
+    </>
   );
 }
