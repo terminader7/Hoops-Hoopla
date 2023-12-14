36e0526e04423fad504cae4b0f327cd1a868572f
Nader Ebrahim <nebrahim2424@gmail.com>
2023-12-14 09:54:57 -0800

    more basic layout stuff, potential page names, set up styeld wrapper component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, styled } from "@mui/material";
 import InlineContainer from "../components/InlineContainer";
 import { green } from "@mui/material/colors";
 import { Global } from "@emotion/react";
+
+export const Wrapper = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  height: 100vh;
+  background-color: black;
+  padding: 0;
+  margin: 0;
+`;
 
 export default function Home() {
   return (
@@ -20,30 +31,28 @@ export default function Home() {
           },
         }}
       />
-      <Box
-        sx={{
-          position: "relative",
-          height: "100vh",
-          backgroundColor: "black",
-          padding: 0,
-          margin: 0,
-        }}
-      >
+      <Wrapper>
         <Box
           sx={{
+            display: "flex",
             height: "100vh",
-            backgroundColor: "green",
-            marginX: "5rem",
+            backgroundColor: "white",
+            flexDirection: "column",
+            width: "75%",
           }}
         >
           <Typography variant="h2" fontWeight={600}>
             Welcome to Hoops Hoopla!
           </Typography>
-          <InlineContainer sx={{ minWidth: "100px", backgroundColor: green }}>
-            <Typography>Hello</Typography>
+          <InlineContainer
+            sx={{ minWidth: "100px", backgroundColor: "#FA8320" }}
+          >
+            <Typography variant="h6" fontWeight={600}>
+              Hello
+            </Typography>
           </InlineContainer>
         </Box>
-      </Box>
+      </Wrapper>
     </>
   );
 }
